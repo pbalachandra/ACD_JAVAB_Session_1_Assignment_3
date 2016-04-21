eea1a09b31280744d7aab1ddab32d16e0eb2fbc5
pbalachandra <balachandra.p@gmail.com>
2016-04-21 14:04:32 +0530

    ACD_JAVAB_Session_1_Assignment_3_Main
    ACD_JAVAB_Session_1_Assignment_3_Main

---
+++ ACD_JAVAB_Session_1_Assignment_3_Main.java
@@ -0,0 +1,24 @@
+/**
+ * @author BALU
+ *
+ */
+package session1;
+
+public class ACD_JAVAB_Session_1_Assignment_3_Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Give two examples of data that cannot be stored in a variable of type int.
+/*Answer : 
+ Examples :
+		int a;
+		a = -2147483649;
+		a =  2147483648;
+
+	*/
+	}
+
+}
